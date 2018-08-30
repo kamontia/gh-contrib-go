f2dd96a2d3133a240967258b8daa0ca5da3cafa4
kondo takeshi <chaspy@users.noreply.github.com>
2018-08-30 21:54:03 +0900

    Add basic code for cli

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
-	"log"
-	"os"
-
+	"fmt"
 	"github.com/urfave/cli"
+	"os"
 )
 
+var GlobalFlags = []cli.Flag{}
+var Commands = []cli.Command{}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}
+
 func main() {
-	err := cli.NewApp().Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = Name
+	app.Version = Version
+	app.Author = "Takeshi Kondo"
+	app.Email = "take.she12@gmail.com"
+	app.Usage = ""
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "user, u",
+			Usage: "Specify github user name",
+		},
+		cli.StringFlag{
+			Name:  "start, s",
+			Usage: "Specify start date. format is YYMMDD",
+		},
+		cli.StringFlag{
+			Name:  "end, e",
+			Usage: "Specify end date. format is YYMMDD",
+		},
 	}
+	app.Commands = Commands
+	app.CommandNotFound = CommandNotFound
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Hello, gh-contrib")
+		return nil
+	}
+	app.Run(os.Args)
 }
